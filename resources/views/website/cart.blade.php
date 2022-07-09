1fd132f8d0c7e912645081bfd9bd5f4bca563250
GustavoDante <gustavodantespfc@gmail.com>
2022-07-08 21:45:58 -0300

    feat: creating cart page and resolve responsives questions

--- resources/views/website/cart.blade.php
+++ resources/views/website/cart.blade.php
@@ -2,14 +2,67 @@
 
 @section('content')
 <div class="container">
-    <div class="container-cart">
-        <div class="logo-product-cart">
-            <img class="img-product-cart" src="https://github.com/GustavoDante.png" alt="">
+    <div class="row">
+        <div class="container-carts col-8">
+            <div class="card-cart">
+                <div class="logo-product-cart">
+                    <img class="img-product-cart" src="https://github.com/GustavoDante.png" alt="">
+                </div>
+                <div class="info-product-cart">
+                    <h1 class="name-product-cart">titulo comum
+                    </h1>
+                    <span class="price">R$39,00</span>
+                </div>
+            </div>
+            <div class="card-cart">
+                <div class="logo-product-cart">
+                    <img class="img-product-cart" src="https://github.com/GustavoDante.png" alt="">
+                </div>
+                <div class="info-product-cart">
+                    <h1 class="name-product-cart">titulo comum
+                    </h1>
+                    <span class="price">R$39,00</span>
+                </div>
+            </div>
+            <div class="card-cart">
+                <div class="logo-product-cart">
+                    <img class="img-product-cart" src="https://github.com/GustavoDante.png" alt="">
+                </div>
+                <div class="info-product-cart">
+                    <h1 class="name-product-cart">titulo comum
+                    </h1>
+                    <span class="price">R$39,00</span>
+                </div>
+            </div>
+            <div class="card-cart">
+                <div class="logo-product-cart">
+                    <img class="img-product-cart" src="https://github.com/GustavoDante.png" alt="">
+                </div>
+                <div class="info-product-cart">
+                    <h1 class="name-product-cart">titulo comum
+                    </h1>
+                    <span class="price">R$39,00</span>
+                </div>
+            </div>
+            <div class="card-cart">
+                <div class="logo-product-cart">
+                    <img class="img-product-cart" src="https://github.com/GustavoDante.png" alt="">
+                </div>
+                <div class="info-product-cart">
+                    <h1 class="name-product-cart">titulo comum
+                    </h1>
+                    <span class="price">R$39,00</span>
+                </div>
+            </div>
         </div>
-        <div class="info-product-cart">
-            <h1 class="name-product-cart">titulo comum
-            </h1>
-            <span class="price">R$39,00</span>
+        <div class="col-4">
+            <div class="card-total-price">
+                <div class="financial-information">
+                    <h1 class="name-product-cart">Total</h1>
+                    <span class="total-price">R$39,00</span>
+                </div>
+                <button class="button-close-sale">Continuar</button>
+            </div>
         </div>
     </div>
 </div>
